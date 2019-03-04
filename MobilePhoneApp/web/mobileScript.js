0c75494fbd3164ea39235a9260f51e39a1518858
Arafa <javaintake@gmail.com>
2019-03-04 20:33:01 +0200

    MobilApp
    First Commit

---
+++ MobilePhoneApp/web/mobileScript.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $("#btnSave").click(function(){
+    //$(this).hide();
+    alert("Save Button Clicked");
+  });
+});
+$(document).ready(function(){
+  $("#btnCancel").click(function(){
+    //$(this).hide();
+    alert("Cancel Button Clicked");
+  });
+});
